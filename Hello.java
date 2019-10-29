0f2346debc909e8808a257733f3ef1c101214007
Arko12 <57125825+Arko12@users.noreply.github.com>
2019-10-29 11:36:18 +0530

    Java with GITHUB
    Java file updated with GITHUB

--- Hello.java
+++ Hello.java
@@ -5,5 +5,9 @@ class Hello
 public static void main(String[]args)
 {
 System.out.println("Hello world:");
+  
+  
+System.out.println("Hi GIT hub");
+System.out.println("This is now entered as first commit in GITHUB");
 }
 }
